8405f3a9e087ac4fce67a01c0b9346c000a714e4
LeviathanS11 <shakiulalamsas11@gmail.com>
2023-05-19 14:12:46 +0600

    navbar added

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/cover.png';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is Header...</h1>
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <img style={{ height: '50px' }} src={logo} alt="" />
+            </div>
+            <div className="navbar-center ">
+                <Link className='ml-2 hover:text-orange-400'>Home</Link>
+                <Link className='ml-2 hover:text-orange-400'>Blog</Link>
+                <Link className='ml-2 hover:text-orange-400'>All Toys</Link>
+                <Link className='ml-2 hover:text-orange-400'>My Toys</Link>
+                <Link className='ml-2 hover:text-orange-400'>Add Toys</Link>
+            </div>
+            <div className="navbar-end">
+                <button>Login</button>
+                <button>Log-Out</button>
+                <img style={{ height: '50px' }} src={logo} alt="" />
+            </div>
         </div>
     );
 };
