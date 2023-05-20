e3b32d73879e146b02a869cbcdc7b6a3feda8940
LeviathanS11 <shakiulalamsas11@gmail.com>
2023-05-20 13:41:01 +0600

    data send to server side

--- src/AddToys/AddToys.jsx
+++ src/AddToys/AddToys.jsx
@@ -8,6 +8,31 @@ const AddToys = () => {
     const handleAddToys=event=>{
         event.preventDefault();
         const form = event.target;
+        const toyName=form.ToyName.value;
+        const name=form.name.value;
+        const category=form.category.value;
+        const email=form.email.value;
+        const price=form.price.value;
+        const rating=form.rating.value;
+        const quantity=form.quantity.value;
+        const detail=form.detail.value;
+        const image=form.image.value
+
+        const all={toyName,name,category,email,price,rating,quantity,detail,image}
+
+        console.log(all)
+
+        fetch('http://localhost:5000/toys',{
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(all)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+        })
     }
 
     return (
@@ -48,7 +73,7 @@ const AddToys = () => {
                             <label className="label">
                                 <span className="label-text">Price</span>
                             </label>
-                            <input type="text" name='Price' required placeholder="Price" className="input input-bordered" />
+                            <input type="text" name='price' required placeholder="Price" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
