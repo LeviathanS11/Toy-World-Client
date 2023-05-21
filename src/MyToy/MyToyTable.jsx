157d8f0a73c78eb8268289a076d684041bb31ed3
LeviathanS11 <shakiulalamsas11@gmail.com>
2023-05-21 12:59:35 +0600

    delete button and method working fine

--- src/MyToy/MyToyTable.jsx
+++ src/MyToy/MyToyTable.jsx
@@ -3,6 +3,18 @@ import { Link } from 'react-router-dom';
 
 const MyToyTable = ({ toy }) => {
     const { name, toyName, category, price, quantity,_id } = toy;
+    const handleDelete=_id=>{
+        const proceed=confirm('are you sure?');
+        if(proceed){
+            fetch(`https://assignment-11-server-leviathans11.vercel.app/toys/${_id}`,{
+                method:'DELETE'
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                console.log(data)
+            })
+        }
+    }
     return (
         <tr>
             <td>
@@ -21,7 +33,7 @@ const MyToyTable = ({ toy }) => {
             <button className="btn btn-ghost btn-xs">Update</button>
             </th>
             <th>
-            <button className="btn btn-ghost btn-xs">Delete</button>
+            <button onClick={()=>handleDelete(_id)} className="btn btn-ghost btn-xs">Delete</button>
             </th>
         </tr>
     );
